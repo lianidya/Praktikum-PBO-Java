6b1a3e3404127c834b9950ba6eb62b02acaf9503
Whdfs23 <danarhusain@gmail.com>
2025-12-12 00:07:06 +0700

    Add PraktikumPBO_9 and Tugas_Praktikum9

---
+++ Tugas_Praktikum9/src/Tugas_Modul/Main_Tugas9.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas_Modul;
+
+public class Main_Tugas9 {
+    public static void main(String[] args) {
+        Hewan kucing = new Kucing();
+        Hewan anjing = new Anjing();
+        kucing.suara();
+        anjing.suara(); 
+    }
+}
